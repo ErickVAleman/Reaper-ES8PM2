0c3d0bd0a4b2d7934256ebf058614375a550b7c7
Erick Aleman <erickva100.eva@gmail.com>
2018-05-29 10:32:07 -0500

    update

--- time.js
+++ time.js
@@ -1,6 +1,6 @@
 'use strict'
-const minute = 1000 *60
-      hour = 1000*60*60
+const minute = 1000 *60,
+      hour = 1000*60*60,
       day = 1000*60*60*24, 
       week = 1000*60*60*24*7, 
       month = 1000*60*60*24*7*4,
